afe7ad3460cd24c23a968785718eefa1b99a884a
Anshul Singh <anshulsingh024@yahoo.com>
2023-10-01 21:12:58 +0530

    Formatted bubble sort file

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter the size of array" << endl;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
+        
         cin >> arr[i];
 
     for (int i = 0; i < n - 1; i++)
@@ -17,7 +19,7 @@ int main()
                 swap(arr[j], arr[j + 1]);
         }
     }
-
+    cout<<"array after bubble sort is : ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
